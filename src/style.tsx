6e37138dadb57cacc1112b812511dd5f878d215f
kkerdyanova <kristina.kerdyanova@gmail.com>
2023-06-09 15:28:26 +0500

    Updates

--- src/style.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const StyledApp = styled.div`
-  width: 100%;
-  margin: auto;
-`;
--- src/ui/colors.ts
+++
@@ -1,9 +0,0 @@
-import { css } from "styled-components";
-
-const beige = css`
-  color: rgba(230, 201, 154, 0.5);
-`;
-
-export const colors = {
-  beige,
-};
