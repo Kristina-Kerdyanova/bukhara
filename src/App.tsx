7e5277856856afe547e664409b5fef1ab60606a1
kkerdyanova <kristina.kerdyanova@gmail.com>
2023-06-14 13:36:07 +0500

    refactor: update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppRouter } from "./router/AppRouter";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StyledApp } from "./style";
 import { GlobalStyle } from "./ui/GlobalStyle";
 import { Styles } from "./ui/Styles";
@@ -10,9 +10,9 @@ export const App = () => {
     <StyledApp>
       <GlobalStyle />
       <Styles/>
-      <BrowserRouter>
+      <HashRouter>
         <AppRouter />
-      </BrowserRouter>
+      </HashRouter>
     </StyledApp>
   );
 };
